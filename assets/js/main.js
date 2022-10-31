3ba74dc6a5008c5c8e3ce1304b4b2edd44a66997
Dario-Miccinelli <poduzzo@hotmail.com>
2022-10-31 15:20:39 +0100

    modalità normale completata

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,23 +6,26 @@ function myFunction() {
 
 function createOuput(){
     const div = document.createElement('div');
+   
     div.classList.add('output');
     return div;
   }
-  
-
  
-  for( let i = 0; i < 100; i++ ){
-
+ 
+  for( let i = 1; i < 101; i++ ){
+    
     let ouputCorrente = createOuput();
+    ouputCorrente.innerHTML = i;
     
   
     ouputCorrente.addEventListener('click', function(){
         console.log(this);
         this.classList.toggle('bg-primary');
+      
     })
   
     griglia.append( ouputCorrente );
+  
   
   }
 }
